a05b45317ab170f28be45afa6eb6add1c251a740
Saahil <neon@saahild.com>
2024-04-03 12:20:50 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"  />
+        <p>
+      <b>Comming soon...</b>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
